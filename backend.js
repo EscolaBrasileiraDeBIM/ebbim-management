fc75139df3ad645f9c3457bf37f2a6fbc69cfb81
gustavosoaresm <gustavo.soaresmatos@gmail.com>
2022-09-14 14:13:02 -0300

    starting

--- backend.js
+++ backend.js
@@ -1,1 +1,5 @@
-teste
+function signIn() {
+    var login = document.getElementById("login").value;
+    var password = document.getElementById("password").value;
+    window.location.href("file.php");
+  }
