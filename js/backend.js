c21a6fccf5f95fe760059a130306edd671ba3b23
gustavosoaresm <gustavo.soaresmatos@gmail.com>
2022-09-30 14:16:48 -0300

    Removido o cadastro

--- js/backend.js
+++ js/backend.js
@@ -16,11 +16,3 @@ function signIn() {
   }
   window.location.href("Controllers/Pessoa/GetPessoa.php?" + userEmail + "=" + login + "&senha=" + password);
 }
-
-function signUp() {
-  var user = getElement("user").value.toString();
-  var email = getElement("email").value.toString();
-  var password = getElement("password").value.toString();
-  window.location.href("Controllers/Pessoa/Inserir.php?usuario" +  + "=" + user + "&senha=" + password + "&email=" + email);
-}
-
