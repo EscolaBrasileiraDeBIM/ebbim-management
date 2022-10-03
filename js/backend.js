1d9b3f8f74bdfdca6db8701fcf7591fe3337c708
gustavosoaresm <gustavo.soaresmatos@gmail.com>
2022-10-03 16:36:19 -0300

    Testando o comando onload

--- js/backend.js
+++ js/backend.js
@@ -3,7 +3,6 @@ function getElement(id) {
 }
 
 function signIn() {
-  alert('Teste');
   var user = getElement("user").value.toString();
   var password = getElement("password").value.toString();
   var xhr = new XMLHttpRequest();
@@ -13,6 +12,9 @@ function signIn() {
       login: user,
       senha: password
   }));
+  xhr.onload = function () {
+    console.log("teste");
+};
 
   //window.location.href("Controllers/Login/Login.php?login=" + login + "&senha=" + password);
 }
