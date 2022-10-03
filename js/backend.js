7f0cd81f4bea84f9ddedbf272f380668d5149be5
gustavosoaresm <gustavo.soaresmatos@gmail.com>
2022-10-03 16:43:25 -0300

    Tentando exibir o responseText do xhr

--- js/backend.js
+++ js/backend.js
@@ -5,11 +5,10 @@ function getElement(id) {
 function signIn() {
   var user = getElement("user").value.toString();
   var password = getElement("password").value.toString();
-  alert(user + " " + password)
   var xhr = new XMLHttpRequest();
   xhr.open("POST", "https://sistema.ebbim.com.br/ebbim-api/Controllers/Login/Login.php", true);
   xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.onload = () => console.log(xhr.responseText);
+  xhr.onload = () => alert(xhr.responseText);
   xhr.send(JSON.stringify({
       login: user,
       senha: password
