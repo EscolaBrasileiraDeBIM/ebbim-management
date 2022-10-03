63b7cf455fe83d6278a249b7601d530ae1e20b39
gustavosoaresm <gustavo.soaresmatos@gmail.com>
2022-10-03 16:41:25 -0300

    Tentando fazer o XMLHTTPRequest funcionar

--- js/backend.js
+++ js/backend.js
@@ -5,16 +5,15 @@ function getElement(id) {
 function signIn() {
   var user = getElement("user").value.toString();
   var password = getElement("password").value.toString();
+  alert(user + " " + password)
   var xhr = new XMLHttpRequest();
   xhr.open("POST", "https://sistema.ebbim.com.br/ebbim-api/Controllers/Login/Login.php", true);
   xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onload = () => console.log(xhr.responseText);
   xhr.send(JSON.stringify({
       login: user,
       senha: password
   }));
-  xhr.onload = function () {
-    console.log("teste");
-};
 
   //window.location.href("Controllers/Login/Login.php?login=" + login + "&senha=" + password);
 }
