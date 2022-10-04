3f86eda27c8dde21c57007f056df854071d31ea3
gustavosoaresm <gustavo.soaresmatos@gmail.com>
2022-10-04 11:36:15 -0300

    Teste

--- js/backend.js
+++ js/backend.js
@@ -1,7 +1,6 @@
 function signIn() {
   var user = document.getElementById("user").value.toString();
   var password = document.getElementById("password").value.toString();
-  alert(user + " " + password);
   var xhr = new XMLHttpRequest();
   xhr.open("POST", "https://sistema.ebbim.com.br/ebbim-api/Controllers/Login/Login.php", true);
   xhr.setRequestHeader('Content-Type', 'application/json');
