f8061594cc153df0c0cb412346f183f5e227f3e3
gustavosoaresm <gustavo.soaresmatos@gmail.com>
2022-10-04 12:20:22 -0300

    Teste

--- js/backend.js
+++ js/backend.js
@@ -2,7 +2,7 @@ function signIn() {
   var user = document.getElementById("user").value.toString();
   var password = document.getElementById("password").value.toString();
   var xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://sistema.ebbim.com.br/ebbim-api/Controllers/Login/Login.php", true);
+  xhr.open("POST", "../../ebbim-api/Controllers/Login/Login.php", true);
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.onreadystatechange = function () {
     if (xhr.readyState == "4") //Request finished and response is ready
