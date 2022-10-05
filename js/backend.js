4e4d5f4661232fd08ece489c14b050320971541a
gustavosoaresm <gustavo.soaresmatos@gmail.com>
2022-10-05 12:21:30 -0300

    Testando o response.json

--- js/backend.js
+++ js/backend.js
@@ -11,7 +11,7 @@ function signIn() {
   }).then((response) => {
     if (response.ok)
     {
-      alert(response.body.getReader())
+      alert(response.json)
       // localStorage.setItem('id', response.body)
       // window.location.assign("https://sistema.ebbim.com.br/html/index.html")
     }
