73e6a8af0739c085f83d9315f59387f6b35f6d81
gustavosoaresm <gustavo.soaresmatos@gmail.com>
2022-10-05 12:34:48 -0300

    XD

--- js/backend.js
+++ js/backend.js
@@ -11,7 +11,7 @@ function signIn() {
   }).then((response) => {
     if (response.ok)
     {
-      alert(response.text)
+      alert(response.body.text())
       // localStorage.setItem('id', response.body)
       // window.location.assign("https://sistema.ebbim.com.br/html/index.html")
     }
