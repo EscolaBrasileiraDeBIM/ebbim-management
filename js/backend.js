8dfc453ed9b318eb2287e885de13def883f3741c
gustavosoaresm <gustavo.soaresmatos@gmail.com>
2022-10-26 11:18:44 -0300

    searchPessoa criado

--- js/backend.js
+++ js/backend.js
@@ -147,3 +147,25 @@ function removePessoa() {
     }
   })
 }
+
+function searchPessoa() {
+  var id = document.getElementById("inputId").value.toString();
+  fetch('https://sistema.ebbim.com.br/ebbim-api/Controllers/Pessoa/GetPessoa.php', {
+  method: "POST",
+  body: new URLSearchParams({
+    'id': id
+  }),
+  headers: {"Content-type": "application/x-www-form-urlencoded"}
+  }).then((response) => {
+    if (response.ok)
+    {
+      response.json().then(function(data) {
+        alert("Encontrado");
+      });
+    }
+    else
+    {
+      alert("Erro");
+    }
+  })
+}
