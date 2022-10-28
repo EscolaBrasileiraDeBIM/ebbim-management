e16a7149c334de3d895dad9d30a351074fb251f5
gustavosoaresm <gustavo.soaresmatos@gmail.com>
2022-10-28 14:07:37 -0300

    Teste

--- js/backend.js
+++ js/backend.js
@@ -168,7 +168,7 @@ function searchPessoa() {
         document.getElementById("inputIe").value = data.ie;
         document.getElementById("inputIm").value = data.im;
         document.getElementById("inputPJ").checked = !isTrue(data.pf);
-        alert(data.pf);
+        alert(isTrue(data.pf) + " " + !isTrue(data.pf));
       });
     }
     else
